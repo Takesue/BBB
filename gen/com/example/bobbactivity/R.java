8281f88a3303cdb279a13769768429f00f2bf0f2
NORIKAZU TAKESUE <uzakiron.27105650@gmail.com>
2012-10-30 14:29:02 +0900

    MK1 1030

--- gen/com/example/bobbactivity/R.java
+++ gen/com/example/bobbactivity/R.java
@@ -11,16 +11,42 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_action_search=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
+        public static final int beetle1=0x7f020000;
+        public static final int beetle2=0x7f020001;
+        public static final int beetle3=0x7f020002;
+        public static final int bobb_bg=0x7f020003;
+        public static final int carddetail_bg=0x7f020004;
+        public static final int fire=0x7f020005;
+        public static final int ic_action_search=0x7f020006;
+        public static final int ic_launcher=0x7f020007;
+        public static final int listitem_bg=0x7f020008;
+        public static final int textarea=0x7f020009;
+        public static final int water=0x7f02000a;
+        public static final int wind=0x7f02000b;
     }
     public static final class id {
-        public static final int button1=0x7f070001;
-        public static final int button2=0x7f070002;
-        public static final int button3=0x7f070003;
-        public static final int button4=0x7f070004;
-        public static final int menu_settings=0x7f070005;
-        public static final int textView1=0x7f070000;
+        public static final int beetleKitDetail=0x7f080004;
+        public static final int beetle_kit_list=0x7f080006;
+        public static final int beetle_kit_selection=0x7f080007;
+        public static final int button1=0x7f080001;
+        public static final int button2=0x7f080002;
+        public static final int button3=0x7f080003;
+        public static final int button4=0x7f080008;
+        public static final int carddetail_atk=0x7f080013;
+        public static final int carddetail_attrribute=0x7f080010;
+        public static final int carddetail_def=0x7f080014;
+        public static final int carddetail_icon=0x7f08000f;
+        public static final int carddetail_intoro=0x7f08000e;
+        public static final int carddetail_name=0x7f080011;
+        public static final int kit_attack=0x7f08000c;
+        public static final int kit_defence=0x7f08000d;
+        public static final int kit_name=0x7f08000b;
+        public static final int kiticon=0x7f08000a;
+        public static final int linearLayout1=0x7f080012;
+        public static final int menu_settings=0x7f080015;
+        public static final int newBeetleKit=0x7f080009;
+        public static final int scrollView1=0x7f080005;
+        public static final int textView1=0x7f080000;
     }
     public static final class layout {
         public static final int activity_battle=0x7f030000;
@@ -36,17 +62,26 @@ public final class R {
         public static final int activity_rule=0x7f03000a;
         public static final int activity_start=0x7f03000b;
         public static final int activity_userinforegistration=0x7f03000c;
+        public static final int beetle_kit_list_item_with_icon=0x7f03000d;
+        public static final int card_detailview=0x7f03000e;
     }
     public static final class menu {
-        public static final int activity_start=0x7f060000;
+        public static final int activity_start=0x7f070000;
+    }
+    public static final class raw {
+        public static final int waoh=0x7f040000;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
-        public static final int hello_world=0x7f040001;
-        public static final int menu_settings=0x7f040002;
-        public static final int title_activity_start=0x7f040003;
+        public static final int app_name=0x7f050000;
+        public static final int hello_world=0x7f050001;
+        public static final int menu_settings=0x7f050002;
+        public static final int title_activity_start=0x7f050003;
     }
     public static final class style {
-        public static final int AppTheme=0x7f050000;
+        /**  
+    <style name="AppTheme" parent="android:Theme.Light" />
+     
+         */
+        public static final int AppTheme=0x7f060000;
     }
 }
